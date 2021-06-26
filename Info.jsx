9ffa4bce1bd4058d69bb0213bc641f6bb1da3408
Vivek Vardhan Maharana <79391371+VivekMaharana87@users.noreply.github.com>
2021-06-27 00:06:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from  "react";
+function Info(){
+  return(
+    <div className="note">
+      <h1 >JavaScript and ReactJs</h1>
+      <p >A basic web development bootcamp using JavaScript and ReactJs.
+        Starts on 30th June,5:30PM IST. Duration - 7days. 
+      </p>
+    </div>
+    );
+}
+export default Info;
